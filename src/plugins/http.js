074c57942d64b6d97ca298e17348b56fc09cc794
Daniel Molina <danmnvx@gmail.com>
2020-06-14 16:37:47 -0500

    Authorization Bearer en cada petición al servidor

--- src/plugins/http.js
+++ src/plugins/http.js
@@ -1,7 +1,35 @@
+import Vue from 'vue'
 import axios from 'axios';
+import router from '../router/index';
 
-export default axios.create({
+const http =  axios.create({
     baseURL: 'http://localhost:3000/api',
-    Headers: { 'Content-Type': 'x-www-form-urlencoded' },
     responseType: 'json',
-})
+});
+
+// Interceptor para mandar el jwt por Autorization en cada petición.
+http.interceptors.request.use(
+    (config) => {
+        // Se obtiene cookie y se almacenca en el encabezado.
+        config.headers = { 'Authorization': `Bearer ${Vue.cookie.get('SSaeI')}` }
+        // Retornamos la configuración.
+        return config;
+    },
+    (err) =>  Promise.reject(err)
+);
+
+// Interceptor para detectar si el token enviado al servidor ya no es válido
+http.interceptors.response.use(
+    (response) => response, // Si no ocurrió ningún error en la petición, se prosigue con la petición
+    (error) => {
+        // Código 401 es único para usuario no autorizado.
+        if (error.response.status == 401) {
+            // Si se obtiene este código, eliminamos la cookie actual con el jwt inválido.
+            Vue.cookie.delete('SSaeI');
+            router.push({ name: 'Login' });
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default http;
