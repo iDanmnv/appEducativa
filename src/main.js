d9b5b17ccad35b202f4071239d02c01c373e3bdf
Daniel Molina <danmnvx@gmail.com>
2020-06-14 00:36:46 -0500

    Uso de vue cookie en app

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueCookie from 'vue-cookie';
 import vuetify from './plugins/vuetify';
 
+Vue.use(VueCookie);
 Vue.config.productionTip = false
 
 new Vue({
