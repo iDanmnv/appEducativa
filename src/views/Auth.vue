fadf59480e8014cb307ad06b2102286d797c9d35
Daniel Molina <danmnvx@gmail.com>
2020-06-14 02:54:16 -0500

    Vista para usuarios no logueados

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,10 +1,48 @@
 <template>
     <v-app>
-        <v-main>
-            <v-container fluid>
-                <router-view></router-view>
+        <v-app-bar
+            fixed
+            color="primary"
+            elevate-on-scroll
+            dark
+        >
+            <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+
+            <v-toolbar-title>App Educativa</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-tooltip bottom>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn icon v-bind="attrs" v-on="on" @click="go('Login')"><v-icon>mdi-login</v-icon></v-btn>
+                </template>
+                <span>Identificarse</span>
+            </v-tooltip>
+            
+
+            <v-tooltip bottom>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn icon v-bind="attrs" v-on="on" @click="go('Register')"><v-icon>mdi-account-plus</v-icon></v-btn>
+                </template>
+                <span>Registrarse</span>
+            </v-tooltip>
+
+            <v-tooltip bottom>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn icon v-bind="attrs" v-on="on" @click="changeTheme"><v-icon>mdi-invert-colors</v-icon></v-btn>
+                </template>
+                <span>Cambiar tema</span>
+            </v-tooltip>
+
+            <!-- <v-btn icon>
+                <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn> -->
+        </v-app-bar>
+        <v-content class="pt-12">
+            <v-container>
+                <router-view class="mt-5"></router-view>
             </v-container>
-        </v-main>
+        </v-content>
     </v-app>
 </template>
 
@@ -15,5 +53,13 @@ export default {
     data: () => ({
         //
     }),
+    methods: {
+        go(pathRoute) {
+            this.$router.push({ name: pathRoute });
+        },
+        changeTheme() {
+            this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+        }
+    }
 };
 </script>
