9864f0bd1ac72c75a1f22dc99ae9e56967893db8
Mario González Santoyo <mario_gnzsa@hotmail.com>
2020-06-18 21:02:21 -0500

    First commit to the branch

--- src/components/dashboard/Account.vue
+++ src/components/dashboard/Account.vue
@@ -1,5 +1,38 @@
 <template>
-    <p>Account</p>
+    <v-container fluid>
+        <v-row
+        class="mb-6"
+        justify="center"
+        no-gutters
+        >
+            <v-col lg="10">
+                <v-card
+                    class="mx-auto"
+                    min-height="500px"
+                    outlined
+                >
+                    <v-list-item three-line>
+                    <v-list-item-content>
+                        <div class="overline mb-4">OVERLINE</div>
+                        <v-list-item-title class="headline mb-1">Headline 5</v-list-item-title>
+                        <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+                    </v-list-item-content>
+
+                    <v-list-item-avatar
+                        tile
+                        size="80"
+                        color="grey"
+                    ></v-list-item-avatar>
+                    </v-list-item>
+
+                    <v-card-actions>
+                    <v-btn text>Button</v-btn>
+                    <v-btn text>Button</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
